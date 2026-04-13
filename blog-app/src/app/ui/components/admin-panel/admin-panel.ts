8ad64f67797d0596d92f22084a8f4fa58fee0050
shpatell <pospeloff2018@yandex.ru>
2026-04-13 17:18:32 +0700

    Начал работу над полным переносом страниц

---
+++ blog-app/src/app/ui/components/admin-panel/admin-panel.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-panel',
+  imports: [],
+  templateUrl: './admin-panel.html',
+  styleUrl: './admin-panel.scss',
+})
+export class AdminPanel {}
