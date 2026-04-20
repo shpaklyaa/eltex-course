84a9bb5353ddb800cee1a367f402913f64913691
shpatell <pospeloff2018@yandex.ru>
2026-04-20 20:37:07 +0700

    UPDATE 4

--- blog-app/src/main.ts
+++ blog-app/src/main.ts
@@ -3,3 +3,12 @@ import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
+
+// "server": "src/main.server.ts",
+            // "outputMode": "server",
+            // "security": {
+            //   "allowedHosts": []
+            // },
+            // "ssr": {
+            //   "entry": "src/server.ts"
+            // }
