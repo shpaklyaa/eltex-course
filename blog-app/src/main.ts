d483ee82dbf4fdb5afe39c05ac37adfbdf70f899
nelepishe <pospeloff2018@yandex.ru>
2026-04-20 18:32:10 +0700

    UPDATE: Lesson 11 (#9)
    * Начал работу над полным переносом страниц
    
    * Перенес страницу блога, настроил роутинг, модифицировал стили для отображения статей, написал несколько компонентов
    
    * Закончил модель модального окна, написал несколько компонентов
    
    * Дописал модальные окна, реализовал удаление статей, отображение формы, подсчет статистики на странице
    
    * Настройка github pages
    
    * UPDATE: исправлен роутинг, изменены настройки pages и несколько мелких доработок

--- blog-app/src/main.ts
+++ blog-app/src/main.ts
@@ -1,11 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { Header } from './app/ui/components/header/header';
-import { Footer } from './app/ui/components/footer/footer';
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
-
-bootstrapApplication(Header, appConfig).catch((err) => console.error(err));
-
-bootstrapApplication(Footer, appConfig).catch((err) => console.error(err));
